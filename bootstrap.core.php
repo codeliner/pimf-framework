48a48825140fe3e119144c02c083fc15e4ac5c0f
Codeliner <kontakt@codeliner.ws>
2014-10-29 19:53:56 +0100

    Check if composer autoloader is available

--- bootstrap.core.php
+++ bootstrap.core.php
@@ -7,5 +7,11 @@
 if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
 if(!defined('BASE_PATH')) define('BASE_PATH', realpath(__DIR__) . DS);
 
-require_once 'autoload.core.php';
-require_once 'utils.php';
+
+// Composer autoloading
+if (file_exists('vendor/autoload.php')) {
+    require_once 'vendor/autoload.php';
+} else {
+    require_once 'autoload.core.php';
+    require_once 'utils.php';
+}
